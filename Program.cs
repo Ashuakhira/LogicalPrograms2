2cb38a9f89bffa76507d906ee1ad10f213288594
Ashuakhira <bashok592@gmail.com>
2023-05-02 12:36:05 +0530

    File added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //onsole.WriteLine("Hello, World!");
+            //PerfectNumber.FindPerfectNumber();
+            // Revesre.ReveseNumber();
+            // Fibonacci.FibonacciSeries();
+            Prime.PrimeNumber();
+        }
+    }
+}
