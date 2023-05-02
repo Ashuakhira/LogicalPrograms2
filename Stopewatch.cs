56c83c2dc95b25325ee7a5f7668a1e869163de09
Ashuakhira <bashok592@gmail.com>
2023-05-02 13:01:18 +0530

    Branch5 is added

---
+++ Stopewatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class stopewatch
+    {
+        public static void Watch()
+        {
+            Console.WriteLine("Press any key to start the stopwatch");
+            Console.ReadKey();
+
+            DateTime startTime = DateTime.Now;
+
+            Console.WriteLine("Stopwatch started...");
+
+            Console.WriteLine("Press any key to stop the stopwatch");
+            Console.ReadKey();
+
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+
+            Console.WriteLine("Elapsed time: {0}", elapsedTime);
+        }
+    }
+}
